3189094737a833516387cc695eaf39589c96d433
Hilgedick, John <John.Hilgedick@disney.com>
2016-04-07 16:14:18 -0700

    trying to get them to talk to each other

--- my-web-app-4/src/main/java/com/ratman/rest/service/PodcastRestService.java
+++ my-web-app-4/src/main/java/com/ratman/rest/service/PodcastRestService.java
@@ -3,8 +3,14 @@ package com.ratman.rest.service;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.ClientConfig;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +22,18 @@ public class PodcastRestService {
 	@GET
 	@Produces({MediaType.TEXT_PLAIN})
 	public String getPodcasts() {
-		return "hello john from my-web-app-4!";
+		
+		ClientConfig clientConfig = new ClientConfig();
+		Client client = ClientBuilder.newClient(clientConfig);
+		
+		WebTarget webTarget = client.target("http://my1:webA/my-web-app/webapi/myresource");
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.TEXT_PLAIN_TYPE);
+		Response response = invocationBuilder.get();
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("status:" + response.getStatus());
+		sb.append("response:" + response.readEntity(String.class));
+		
+		return sb.toString();
 	}
 }
