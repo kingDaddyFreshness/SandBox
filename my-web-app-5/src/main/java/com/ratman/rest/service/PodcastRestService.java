c9c3f4c6a7e28aaffc97bed8c40f050bbacffff3
Hilgedick, John <John.Hilgedick@disney.com>
2016-04-10 20:02:00 -0700

    update my-app-5 to be just like 4

--- my-web-app-5/src/main/java/com/ratman/rest/service/PodcastRestService.java
+++ my-web-app-5/src/main/java/com/ratman/rest/service/PodcastRestService.java
@@ -3,19 +3,40 @@ package com.ratman.rest.service;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.ClientConfig;
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/podcasts")
 public class PodcastRestService {
 	
-	
-
 	@GET
 	@Produces({MediaType.TEXT_PLAIN})
 	public String getPodcasts() {
-		return "hello john from my-web-app-5!";
+		
+		String hostname = System.getenv("HOSTNAME");
+		
+		ClientConfig clientConfig = new ClientConfig();
+		Client client = ClientBuilder.newClient(clientConfig);
+		
+		String url = "http://webA:8080/my-web-app/webapi/myresource";
+		System.out.println("url:" + url);
+		
+		WebTarget webTarget = client.target(url);
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.TEXT_PLAIN_TYPE);
+		Response response = invocationBuilder.get();
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("status:" + response.getStatus());
+		sb.append("response:" + response.readEntity(String.class));
+		
+		return sb.toString();
 	}
 }
